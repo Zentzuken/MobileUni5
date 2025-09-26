23891ec9c488137317bb6a06571cf8b6864fcd47
Brian A J Suntpiet <brian_placeholder@Ferdys-MacBook-Air.local>
2025-09-26 13:21:14 +0700

    minor widgets updates

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter App',
       theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.blue),
       home: const HomeScreen(),
     );
